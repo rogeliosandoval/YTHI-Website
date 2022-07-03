971345a2b27bc00627d3228382e464612081491b
Roger <rogelio.g.sandoval@gmail.com>
2022-07-02 22:15:59 -0500

    add more content to the home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  certs = [
+    {imgUrl:'c1'},
+    {imgUrl:'c2'},
+    {imgUrl:'c3'},
+    {imgUrl:'c4'},
+    {imgUrl:'c5'},
+    {imgUrl:'c6'},
+    {imgUrl:'c7'},
+    {imgUrl:'c8'},
+    {imgUrl:'c9'},
+    {imgUrl:'c10'},
+    {imgUrl:'c11'},
+    {imgUrl:'c12'},
+    {imgUrl:'c13'},
+    {imgUrl:'c14'},
+    {imgUrl:'c15'}
+  ];
 
   constructor() { }
 
