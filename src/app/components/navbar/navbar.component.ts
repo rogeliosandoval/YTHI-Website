d6ee7392edd5dacb4f76c3d65480924c3b947d54
Roger <rogelio.g.sandoval@gmail.com>
2022-07-03 23:53:59 -0500

    add decorator

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  navChange = false;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  currentPosition = window.pageYOffset;
+  @HostListener('window:scroll', ['$event.target']) // for window scroll events
+  scroll(e: { scrollingElement: { scrollTop: any; }; }) {
+    let scroll = e.scrollingElement.scrollTop;
+    if (scroll > 1) {
+      this.navChange = true;
+    } else {
+      this.navChange = false;
+    }
   }
 
 }
