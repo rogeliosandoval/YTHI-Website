4d7b6a2e2c5a4852c6b4a7804543335efe48369e
Roger <rogelio.g.sandoval@gmail.com>
2022-07-11 13:12:56 -0500

    create careers component

---
+++ src/app/components/careers/careers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'careers',
+    templateUrl: './careers.component.html',
+    styleUrls: ['./careers.component.css']
+})
+
+export class Careers {
+
+}
