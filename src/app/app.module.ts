091be796f8a2b5ab1c9ed562b886d2a15dfd4e1f
Roger <rogelio.g.sandoval@gmail.com>
2022-07-14 23:38:57 -0500

    update the DOM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router'
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { Reviews } from './components/reviews/reviews.component';
+import { Services } from './components/services/services.component';
+import { Careers } from './components/careers/careers.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'reviews', component: Reviews },
+  { path: 'services', component: Services },
+  { path: 'careers', component: Careers }
 ];
 
 @NgModule({
@@ -18,12 +26,17 @@ const routes: Routes = [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    Reviews,
+    Services,
+    Careers
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
-    NgbModule
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
