95aae87e978641cb24f10597503df92c98ae3ab9
Roger <rogelio.g.sandoval@gmail.com>
2022-07-20 23:31:13 -0500

    create about component

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
+})
+
+export class About {
+    certs = [
+        {imgUrl:'c1'},
+        {imgUrl:'c2'},
+        {imgUrl:'c3'},
+        {imgUrl:'c4'},
+        {imgUrl:'c5'},
+        {imgUrl:'c6'},
+        {imgUrl:'c7'},
+        {imgUrl:'c8'},
+        {imgUrl:'c9'},
+        {imgUrl:'c10'},
+        {imgUrl:'c11'},
+        {imgUrl:'c12'},
+        {imgUrl:'c13'},
+        {imgUrl:'c14'},
+        {imgUrl:'c15'}
+      ];
+}
