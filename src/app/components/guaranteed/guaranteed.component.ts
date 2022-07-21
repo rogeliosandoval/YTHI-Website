221fe7e0621f180776a9c3dde024a71f1f99f5c5
Roger <rogelio.g.sandoval@gmail.com>
2022-07-21 15:33:15 -0500

    create guaranteed component

---
+++ src/app/components/guaranteed/guaranteed.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'guaranteed',
+    templateUrl: 'guaranteed.component.html',
+    styleUrls: ['./guaranteed.component.css']
+})
+
+export class Guaranteed {
+    certs = [
+        {imgUrl:'c1'},
+        {imgUrl:'c2'},
+        {imgUrl:'c3'},
+        {imgUrl:'c4'},
+        {imgUrl:'c5'},
+        {imgUrl:'c6'},
+        {imgUrl:'c7'},
+        {imgUrl:'c8'},
+        {imgUrl:'c9'},
+        {imgUrl:'c10'},
+        {imgUrl:'c11'},
+        {imgUrl:'c12'},
+        {imgUrl:'c13'},
+        {imgUrl:'c14'},
+        {imgUrl:'c15'}
+      ];
+}
